0f2bce1edc467ce4ec62cc99a402bdc9a8ee1124
DYRIX\Graysire <graysire@dyrix.net>
2018-10-11 16:46:03 -0700

    Added a few boxes

---
+++ Train Of Thought/Assets/MovementControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControl : MonoBehaviour
+{
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(speed * Time.deltaTime * horizontalInput, 0);
+	}
+}
