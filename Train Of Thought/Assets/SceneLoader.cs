b5a0a24d0b7a8e2b4a4eaddbc30ad9c86e0f1b5e
DYRIX\Graysire <graysire@dyrix.net>
2018-10-12 13:18:57 -0700

    Added placeholder main menu w/ play button

---
+++ Train Of Thought/Assets/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    //loads a scene at build ndex
+	public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+	}
+}
