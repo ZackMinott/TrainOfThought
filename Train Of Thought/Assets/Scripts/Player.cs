fceb20d8047ad2e25865eb88da1ec9847d6b64f0
Kim Marzo <kjrmarzo@gmail.com>
2018-10-16 14:33:05 -0400

    Added: * Ground checking * Starts of with the shadow character

--- Train Of Thought/Assets/Scripts/Player.cs
+++ Train Of Thought/Assets/Scripts/Player.cs
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour {
 
 	private float speedMultiplier = 1.5f;
 	private float jumpMultiplier = 2.5f;
-	private bool grounded = false;
+    private bool isGround = true;
 	private bool inLight = false;
 	private SpriteRenderer spriteRenderer;
 
@@ -25,19 +25,29 @@ public class Player : MonoBehaviour {
 		rb2d = GetComponent<Rigidbody2D> ();
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 		if (spriteRenderer.sprite == null)
-			spriteRenderer.sprite = norm.GetComponent<SpriteRenderer> ().sprite;
+			spriteRenderer.sprite = shadow.GetComponent<SpriteRenderer> ().sprite;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // For checking ground
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            isGround = true;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
 		//movement
 		float movement = Input.GetAxis ("Horizontal");
 		rb2d.velocity = new Vector2(movement * speed, rb2d.velocity.y);
 
 
 		//Jump
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.Space) && isGround == true) {
 			rb2d.AddForce (transform.up * jumpVelocity);
+            isGround = false;
+            
 		}
 
 		//Change Sprite
@@ -53,7 +63,8 @@ public class Player : MonoBehaviour {
 			spriteRenderer.sprite = shadow.GetComponent<SpriteRenderer> ().sprite; //sets sprite image to shadow
 			speed *= speedMultiplier; //sets speed to prefab
 			jumpVelocity *= jumpMultiplier; //sets jump height to prefab
-		} else {
+		}
+        else {
 			spriteRenderer.sprite = norm.GetComponent<SpriteRenderer> ().sprite; //sets sprite image to normal boy
 			speed /= speedMultiplier; 
 			jumpVelocity /= jumpMultiplier;
