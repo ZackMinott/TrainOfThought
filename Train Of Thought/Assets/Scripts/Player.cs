85606bf934605e14f0269560dfb0ccc2dd5d58b2
Zack Minott <zacharyminott1997@gmail.com>
2018-10-17 18:08:27 -0700

    ReOrganized Switching, Light Switching DONE, no horizontal movement for shadow DONE, rigidbody not sticking to wall DONE(added physics component to obstacle)

--- Train Of Thought/Assets/Scripts/Player.cs
+++ Train Of Thought/Assets/Scripts/Player.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    [Header("Normal Attributes")]
+	[Header("Normal Attributes")]
 	public float normalSpeed;
 	public float normalJumpVelocity;
-    [Header("Shadow Attributes")]
-    public float shadowSpeed;
-    public float shadowJumpVelocity;
-    public GameObject norm;
+	[Header("Shadow Attributes")]
+	public float shadowSpeed;
+	public float shadowJumpVelocity;
+	public GameObject norm;
 	public GameObject shadow;
+	[HideInInspector]
+	public bool isNormalForm = true;
 
-    public bool isNormalForm = true;
-    private bool isGround = true;
-	public bool inLight;
+    private bool isGrounded = true;
+	private bool inLight = false;
 	private SpriteRenderer spriteRenderer;
 
 	Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Awake () {
-		rb2d = GetComponent<Rigidbody2D> ();
-		spriteRenderer = GetComponent<SpriteRenderer> ();
-		if (spriteRenderer.sprite == null)
-			spriteRenderer.sprite = shadow.GetComponent<SpriteRenderer> ().sprite;
+		//Instantiate (norm, new Vector2(this.transform.position.x, this.transform.position.y));
+		rb2d = GetComponent<Rigidbody2D> ();		
 	}
 
     // For checking ground
@@ -33,73 +32,85 @@ public class Player : MonoBehaviour {
     {
         if (collision.gameObject.tag == "ground")
         {
-            isGround = true;
+            isGrounded = true;
         }
     }
 
-    //For checking light
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "lightsource")
-        {
-          inLight = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.gameObject.tag == "lightsource")
-        {
-            isNormalForm = true;
-            inLight = false;
-        }
-    }
-
-
-
-
-    void Update () {
-        if (isNormalForm)
-        {
-            //normal movement
-            float normalMovement = Input.GetAxis("Horizontal");
-            transform.position = transform.position + new Vector3(normalMovement * normalSpeed * Time.deltaTime, 0, 0);
-
-
-            //normal jump
-            if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
-            {
-                rb2d.AddForce(new Vector2(0, normalJumpVelocity), ForceMode2D.Impulse);
-                isGround = false;
-            }
-            //switching to specific area only
-            if (Input.GetKeyDown(KeyCode.E) && inLight == true)
-            {
-                isNormalForm = false;
-            }
-
-
-        }
-        else
-        {
-            //shadow movement
-            if (isGround == true)
-            {
-            float shadowMovement = Input.GetAxis("Horizontal");
-            transform.position = transform.position + new Vector3(shadowMovement * shadowSpeed * Time.deltaTime, 0, 0);
-            }
-            //shadow jump
-            if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
-            {
-                rb2d.AddForce(new Vector2(0, shadowJumpVelocity), ForceMode2D.Impulse);
-                isGround = false;
-            }
-            //switching to specific area only
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                isNormalForm = true;
-            }
-
-        }
+	//for checking light, allows to turn into shadow
+	private void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "lightsource") {
+			inLight = true;
+		}
 	}
+
+	//for exiting light
+	private void OnTriggerExit2D(Collider2D col){
+		if (col.gameObject.tag == "lightsource") {
+			inLight = false; 
+			if (!isNormalForm) //switches back to normal when leaving light source
+				PlayerSwitch ();
+		}
+	}
+
+    // Update is called once per frame
+    void Update () {
+		//movement
+		float movement = Input.GetAxis ("Horizontal");
+
+		if (isNormalForm) {
+			rb2d.velocity = new Vector2 (movement * normalSpeed, rb2d.velocity.y); //Normal Speed
+			Jump (normalJumpVelocity); //normal jump
+			if(Input.GetKeyDown(KeyCode.E))
+				PlayerSwitch();
+		} else if (isNormalForm == false) {
+			if (isGrounded)
+				rb2d.velocity = new Vector2 (movement * shadowSpeed, rb2d.velocity.y); //Shadow Speed
+			else if (!isGrounded)
+				rb2d.velocity = new Vector2 (0, rb2d.velocity.y);
+			Jump(shadowJumpVelocity); //shadow jump
+			if(Input.GetKeyDown(KeyCode.E))
+				PlayerSwitch();
+		}
+
+
+	}
+
+	//Game Object change
+	void PlayerSwitch(){
+		if (isNormalForm && inLight) {
+			isNormalForm = false;
+			norm.SetActive (false);
+			shadow.SetActive (true);
+		} else if (isNormalForm == false) {
+			isNormalForm = true;
+			shadow.SetActive (false);
+			norm.SetActive (true);
+		}
+	}
+
+	/*
+	//Sprite Change Function
+	void ChangeSprite(){
+		if (spriteRenderer.sprite == norm.GetComponent<SpriteRenderer> ().sprite) {
+			spriteRenderer.sprite = shadow.GetComponent<SpriteRenderer> ().sprite; //sets sprite image to shadow
+			speed *= speedMultiplier; //sets speed to prefab
+			jumpVelocity *= jumpMultiplier; //sets jump height to prefab
+		}
+        else {
+			speed /= speedMultiplier; 
+			jumpVelocity /= jumpMultiplier;
+		}
+	}
+	*/
+
+	//Jumping Mechanic
+	void Jump(float jumpVelocity){
+		if (Input.GetKeyDown (KeyCode.Space) && isGrounded) {
+			rb2d.AddForce (transform.up * jumpVelocity);
+			isGrounded = false;
+		}
+	}
+
+
 
 }
