5e14a4eb3aab7e8a4478eaef7146c4f5338f06d4
Zack Minott <zacharyminott1997@gmail.com>
2018-10-24 18:03:30 -0700

    Added Player Switching in Light, and Cinemachine camera follow

--- Train Of Thought/Assets/Scripts/ImportantScripts/Player1.cs
+++ Train Of Thought/Assets/Scripts/ImportantScripts/Player1.cs
@@ -4,27 +4,47 @@ using UnityEngine;
 
 [RequireComponent(typeof(PlayerController))] //Automatically adds required component to gameObject
 public class Player1 : MonoBehaviour {
-    public float jumpHeight = 4; //how high we want the character to jump
-    //ALTER TIME TO JUMP FOR SHADOW
-    public float timeToJump = .4f; //how long character takes to reach highest point
+    [Header("Normal Atrributes")]
+    public float normalJumpHeight = 4; //how high we want the character to jump
+    public float normalTimeToJump = .4f; //how long character takes to reach highest point
+    public float normalMoveSpeed = 6;
+    [Header("Shadow Attributes")]
+    public float shadowJumpHeight = 7;
+    public float shadowTimeToJump = .6f;
+    public float shadowMoveSpeed = 12;
+
+    public GameObject norm;
+    public GameObject shadow;
+
+    [HideInInspector]
+    public bool isNormalForm = true;
+    private bool isGrounded = true;
+    private bool inLight = false;
+
     float accelerationTimeAir = .2f; //time it takes to move while in the air
     float accelerationTimeGrounded = .1f; //time it takes to move/switch directions while grounded
-    //ALTER MOVE SPEED FOR SHADOW
-    public float moveSpeed = 6;
-
-    float gravity;
-    float jumpVelocity;
+    
+    float normGravity;
+    float shadowGravity;
+    float normalJumpVelocity;
+    float shadowJumpVelocity;
     Vector3 velocity;
     float velocityXSmoothing;
 
     PlayerController controller;
+    Rigidbody2D rb2d;
 
     private void Start()
     {
         controller = GetComponent<PlayerController>(); //grabs playerController component
+        rb2d = GetComponent<Rigidbody2D>(); 
 
-        gravity = (-2 * jumpHeight) / Mathf.Pow(timeToJump, 2);
-        jumpVelocity = Mathf.Abs(gravity) * timeToJump;
+        //sets the gravity
+        normGravity = (-2 * normalJumpHeight) / Mathf.Pow(normalTimeToJump, 2);
+        shadowGravity = (-2 * shadowJumpHeight) / Mathf.Pow(shadowTimeToJump, 2);
+        //sets the jump velocity by multiplying the time to jump by velocity
+        normalJumpVelocity = Mathf.Abs(normGravity) * normalTimeToJump;
+        shadowJumpVelocity = Mathf.Abs(shadowGravity) * shadowTimeToJump;
     }
 
     private void Update()
@@ -37,16 +57,85 @@ public class Player1 : MonoBehaviour {
 
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         
-        if(Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+
+        float targetVelocityX = 0;
+        //WORK WITH THIS
+        if (isNormalForm)
+        {
+            targetVelocityX = input.x * normalMoveSpeed;
+            Jump(normalJumpVelocity); //normal jump
+            if (Input.GetKeyDown(KeyCode.E))
+                PlayerSwitch();
+        }
+        else if (isNormalForm == false)
+        {
+            targetVelocityX = input.x * shadowMoveSpeed;
+            if(!controller.collisions.below)
+                targetVelocityX = input.x * 0;
+            Jump(shadowJumpVelocity); //shadow jump
+            if (Input.GetKeyDown(KeyCode.E))
+                PlayerSwitch();
+        }
+        //WORK WITH THIS
+
+        
+        //Smooth Damp gradually changes a value towards a desired goal over time
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)? accelerationTimeGrounded:0); //Smooths the players movement on switching directions
+        if (isNormalForm)
+        {
+            velocity.y += normGravity * Time.deltaTime; //applies gravity to velocity
+        } else if (!isNormalForm)
+        {
+            velocity.y += shadowGravity * Time.deltaTime; //applies gravity to velocity
+        }
+        
+        controller.Move(velocity * Time.deltaTime);
+
+
+    }
+
+    void PlayerSwitch()
+    {
+        if (isNormalForm && inLight)
+        {
+            isNormalForm = false;
+            norm.SetActive(false);
+            shadow.SetActive(true);
+        }
+        else if (isNormalForm == false)
+        {
+            isNormalForm = true;
+            shadow.SetActive(false);
+            norm.SetActive(true);
+        }
+    }
+
+    void Jump(float jumpVelocity)
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
         {
             velocity.y = jumpVelocity;
         }
+    }
 
-        float targetVelocityX = input.x * moveSpeed;
-        //Smooth Damp gradually changes a value towards a desired goal over time
-        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)? accelerationTimeGrounded:accelerationTimeAir); //Smooths the players movement on switching directions
-        velocity.y += gravity * Time.deltaTime; //applies gravity to velocity
-        controller.Move(velocity * Time.deltaTime);
+    //for checking light, allows to turn into shadow
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "lightsource")
+        {
+            inLight = true;
+        }
+    }
+
+    //for exiting light
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "lightsource")
+        {
+            inLight = false;
+            if (!isNormalForm) //switches back to normal when leaving light source
+                PlayerSwitch();
+        }
     }
 
 }
