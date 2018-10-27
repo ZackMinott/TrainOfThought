481a55387b2ca4cef9f4c5cf8da0206879919dc7
DYRIX\Graysire <graysire@dyrix.net>
2018-10-27 13:08:22 -0700

    Added a basic button and event manager, could use some work and needs an animation, but it works

---
+++ Train Of Thought/Assets/Scripts/ButtonLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonLight : MonoBehaviour {
+
+    public GameObject button;
+    public GameObject lightSource;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (button.GetComponent<ButtonScript>().getPressed())
+        {
+            lightSource.SetActive(true);
+        }
+	}
+}
---
+++ Train Of Thought/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+
+    public bool pressDirectionPositive = true; //the direction the button needs to be pressed from, positive on the axis or negative on the axis
+    public bool isXButton = false; //the axis the ubtton operates on, True for X Axis, false for Y Axis
+    public float distance = 1.0f; //how far the button travels when pressed
+    private bool isPushed = false; //whether the button has been pushed
+
+    // Use this for initialization
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (pressDirectionPositive && isXButton)
+        {
+            if (collision.transform.position.x > transform.position.x)
+            {
+                transform.Translate(new Vector3(-distance, 0));
+                isPushed = true;
+            }
+        }
+        else if (!pressDirectionPositive && isXButton)
+        {
+            if (collision.transform.position.x < transform.position.x)
+            {
+                transform.Translate(new Vector3(distance, 0));
+                isPushed = true;
+            }
+        }
+        else if (pressDirectionPositive && !isXButton)
+        {
+            if (collision.transform.position.y > transform.position.y)
+            {
+                transform.Translate(new Vector3(0, -distance));
+                isPushed = true;
+            }
+        }
+        else if (!pressDirectionPositive && !isXButton)
+        {
+            if (collision.transform.position.y < transform.position.y)
+            {
+                transform.Translate(new Vector3(0, distance));
+                isPushed = true;
+            }
+        }
+    }
+
+    public bool getPressed()
+    {
+        return isPushed;
+    }
+}
