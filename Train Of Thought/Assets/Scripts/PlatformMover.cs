9241a4a4eda1d600b03d642f59c8230744c54e0a
Graysire <graysire@dyrix.net>
2018-11-08 21:47:20 -0800

    Added platform moving script

---
+++ Train Of Thought/Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))] //adds boxCollider2D to gameObject
+public class PlatformMover : MonoBehaviour {
+
+    public LayerMask passengerMask;
+    public LayerMask collisionMask; //determines which objects we want to collide with
+
+    const float skinWidth = .015f;
+
+    [Header("Rays Being Fired")]
+    public int horizontalRayCount = 4; //Amount of rays being fired horizontally
+    public int verticalRayCount = 4; //Amount of rays being fired vertically
+
+    public Vector3 speed; //speed of the object
+
+    //Calculates spacing between each ray
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+
+    BoxCollider2D collider;
+    RaycastOrigins raycastOrigins;
+    public CollisionInfo collisions;
+
+    private void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+
+        CalculateRaySpacing();
+    }
+
+    public void Update()
+    {
+        UpdateRaycastOrigins();
+
+        MovePassengers(speed * Time.deltaTime);
+        transform.Translate(speed * Time.deltaTime);
+    }
+
+    void MovePassengers(Vector3 velocity)
+    {
+        HashSet<Transform> movedPassengers = new HashSet<Transform>();
+
+        float directionX = Mathf.Sign(velocity.x);
+        float directionY = Mathf.Sign(velocity.y);
+
+        //vertically moving platform
+        if (velocity.y != 0)
+        {
+            float rayLength = Mathf.Abs(velocity.y) + skinWidth; //grabs length of the ray
+
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft; //if moving down set raycast origins to bottomleft and if moving up set raycast origins to top left
+                rayOrigin += Vector2.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, passengerMask);
+
+                if (hit) //if the raycast hits something
+                {
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = (directionY == 1) ? velocity.x : 0;
+                        float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
+
+                        hit.transform.Translate(new Vector3(pushX, pushY));
+                    }
+                }
+            }
+        }
+
+        //horizontally moving platform
+        if (velocity.x != 0)
+        {
+            float rayLength = Mathf.Abs(velocity.x) + skinWidth; //grabs length of the ray
+
+            for (int i = 0; i < horizontalRayCount; i++)
+            {
+                Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight; //if moving down set raycast origins to bottomleft and if moving up set raycast origins to top left
+                rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
+
+                if (hit) //if the raycast hits something
+                {
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
+                        float pushY = 0;
+
+                        hit.transform.Translate(new Vector3(pushX, pushY));
+                    }
+                }
+            }
+        }
+
+        // passenger is on top of horizontally or downward moving platform
+
+        if (directionY == -1 || velocity.y == 0 && velocity.x != 0)
+        {
+            float rayLength = skinWidth * 2; //grabs length of the ray
+
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i); //if moving down set raycast origins to bottomleft and if moving up set raycast origins to top left
+    
+                RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+                if (hit) //if the raycast hits something
+                {
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = velocity.x;
+                        float pushY = velocity.y;
+
+                        hit.transform.Translate(new Vector3(pushX, pushY));
+                    }
+                }
+            }
+        }
+    }
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds; //gets bounds of collider
+        bounds.Expand(skinWidth * -2); //shrinks the bounds on all sides by the skinWidth
+
+        //Sets all the bounds of the collider
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds; //gets bounds of collider
+        bounds.Expand(skinWidth * -2); //shrinks the bounds on all sides by the skinWidth
+
+        //sets a minimum of at least two rays being fired in both directions
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1); //spacing would be entire length of the bounds if min value
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    //holds data that will not be modified later
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public bool climbingSlope;
+        public bool descendingSlope;
+        public float slopeAngle, slopeAngleOld;
+        public Vector3 velocityOld;
+
+        //resets all bools to false
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+            climbingSlope = false;
+            descendingSlope = false;
+
+            slopeAngleOld = slopeAngle;
+        }
+    }
+}
