ce526b69bd14391031f0ae05c612fd167b9d77ae
ZackMinott <zacharyminott1997@gmail.com>
2018-11-13 10:19:09 -0800

    GlassWall bug fixed

---
+++ Train Of Thought/Assets/Scripts/ImportantScripts/InGlassWall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGlassWall : MonoBehaviour {
+
+    Player1 player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<Player1>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        player.inGlass = true;
+        Debug.Log("InGlass: " + player.inGlass);
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            player.inGlass = false;
+        }
+    }
+}
