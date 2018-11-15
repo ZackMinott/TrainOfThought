48362d2b5c96ab27bb6173b1ff38c55386832e40
Kim Marzo <kjrmarzo@gmail.com>
2018-11-14 22:34:06 -0500

    Intro ft. (draft) Still needs the ff: - Switch Animation - Text for key instructions

---
+++ Train Of Thought/Assets/FireFliesStartSAOrder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFliesStartSAOrder : MonoBehaviour {
+    public GameObject FireFliesStart;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("FFStartTimer");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator FFStartTimer()
+    {
+
+        yield return new WaitForSeconds(1);
+        Debug.Log("ff done");
+    }
+}
---
+++ Train Of Thought/Assets/Scripts/DelayedStart.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedStart : MonoBehaviour {
+    public GameObject PlayerController;
+    public GameObject FFStart;
+    public GameObject FFFormBox;
+    public GameObject FFEnd;
+    public GameObject FFLight;
+    public GameObject Shadow;
+    public GameObject Normal;
+    public bool goPlayEnd = false;
+ 
+    // Use this for initialization
+    void Start () {
+        StartCoroutine("StartDelay");
+       
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Horizontal") && goPlayEnd == true)
+        {
+            FFFormBox.SetActive(false);
+            FFEnd.SetActive(true);
+            Normal.SetActive(true);
+            Shadow.SetActive(false);
+            FFLight.SetActive(false);
+        }
+
+    }
+
+    IEnumerator StartDelay()
+    {
+        
+        yield return new WaitForSeconds(1);
+        Debug.Log("waited");
+        PlayIdle();
+        
+    }
+    public void PlayIdle()
+    {
+        FFFormBox.SetActive(true);
+        Shadow.SetActive(true);
+        Normal.SetActive(false);
+        FFLight.SetActive(true);
+        StartCoroutine("FFFormBoxDelay");
+    }
+
+    IEnumerator FFFormBoxDelay()
+    {
+        yield return new WaitForSeconds(1);
+        PlayerController.GetComponent<Player1>().enabled = true;
+        goPlayEnd = true;
+    }
+   
+}
