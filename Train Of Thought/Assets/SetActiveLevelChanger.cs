ab6aff7a871310c12b19ed285f0ba6049624c6a1
LAPTOP-V5E93NNM\steve <stevencenteno13@gmail.com>
2018-12-05 22:41:09 -0800

    Implemented scene Darkening + Portal Implemented

---
+++ Train Of Thought/Assets/SetActiveLevelChanger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveLevelChanger : MonoBehaviour {
+
+    public LevelFade LevelFade;
+    public PlayGame PlayGame;
+    public GameObject LevelFading;
+
+    private bool ActiveLevelFade = true;
+
+    private void Start()
+    {
+        LevelFade.BeginAnimation = true;
+    }
+    // Update is called once per frame
+    void Update () {
+        bool buttonPressed = PlayGame.GameStart;
+        bool AnimationDone = LevelFade.animationfinished;
+        if (LevelFade.FadeCompleted)
+        {
+            PlayGame.GameStart = false;
+        }
+        if (AnimationDone)
+        {
+            AnimationFinished();
+        }
+        if (buttonPressed)
+        {
+            ButtonPressed();
+        }
+        if (ActiveLevelFade)
+        {
+            ActiveLevelFade = false;
+            LevelFade.animationfinished = false;
+            LevelFading.SetActive(true);
+        }
+	}
+    public void ButtonPressed()
+    {
+        ActiveLevelFade = true;
+    }
+    public void AnimationFinished()
+    {
+        LevelFading.SetActive(false);
+    }
+    
+}
