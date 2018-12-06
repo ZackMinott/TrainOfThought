398003ad9dcafa9b58aaba573e272a86d85680e7
ZackMinott <zacharyminott1997@gmail.com>
2018-12-05 18:21:40 -0800

    More Level2 design

---
+++ Train Of Thought/Assets/DestroyTrap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTrap : MonoBehaviour {
+    [SerializeField] GameObject button;
+
+    ButtonScript buttonPressed;
+
+    void Start()
+    {
+        buttonPressed = button.GetComponent<ButtonScript>();
+    }
+
+    void Update()
+    {
+        if(buttonPressed.isPushed == true)
+        {
+            StartCoroutine("destruction");
+        }
+    }
+
+    IEnumerator destruction()
+    {
+        yield return new WaitForSeconds(6f);
+        Destroy(gameObject);
+    }
+}
