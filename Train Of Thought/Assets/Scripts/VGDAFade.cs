7809785b9a984191bbc70abad948a0a7e23ba578
Graysire <graysire@dyrix.net>
2018-12-06 09:53:21 -0800

    Reconnected options menu to volume, added VGDA fade to main menu

---
+++ Train Of Thought/Assets/Scripts/VGDAFade.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))] //Automatically adds required component to gameObject
+public class VGDAFade : MonoBehaviour {
+
+
+    SpriteRenderer Renderer;
+    public GameObject FadeAfter;
+    bool Faded = false;
+	// Use this for initialization
+	void Start ()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Renderer.color.a > 0 && FadeAfter != null? FadeAfter.GetComponent<VGDAFade>().GetFaded() : true)
+        {
+            Renderer.color = new Color(Renderer.color.r, Renderer.color.g, Renderer.color.b, Renderer.color.a - .005f);
+        }
+        if (Renderer.color.a <= 0)
+        {
+            Faded = true;
+        }
+	}
+
+    public bool GetFaded()
+    {
+        return Faded;
+    }
+}
